619cc150d7318f82a37656408b222014aca11ea8
TenshiAngelina <angelina.hcortes@gmail.com>
2023-05-19 13:48:49 -0600

    Add prompts to create roles

--- db/index.js
+++ db/index.js
@@ -4,27 +4,33 @@ const connection = require("./connection");
 function findAllEmployees() {
   return connection.promise().query("SELECT * FROM employees");
 }
+// Function that returns all departments
 function findAllDepartments() {
   return connection.promise().query("SELECT * FROM departments");
 }
+// Function that returns all roles
 function findAllRoles() {
   return connection.promise().query("SELECT * FROM roles");
 }
-
+// Function that creates a new department
 function insertDepartment(departmentname) {
   return connection
     .promise()
     .query(`INSERT INTO departments (name) VALUES (${departmentname});`);
 }
+// Function that creates a new role
 function insertRole(title,salary,department_id) {
   return connection.promise().query(`INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`,[title,salary,department_id]);
 }
+// Function that creates a new employee
 function insertEmployee(first_name, last_name, role_id, manager_id) {
   return connection.promise().query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`,[first_name, last_name, role_id, manager_id]);
 }
+// Function that ends the app
 function closeConection(){
   return connection.end()
 }
+
 module.exports = {
   findAllEmployees,
   findAllDepartments,
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 require("console.table");
 const db = require("./db");
-
+// Initial function, it prompts initial question
 function init() {
   inquirer
     .prompt([
@@ -10,12 +10,12 @@ function init() {
         message: "Welcome to the employee tracker!",
         name: "menu",
         choices: [
-          "Add Employee",
-          "Add role",
-          "Add Department",
           "View all employees",
           "View all departments",
           "View all roles",
+          "Add Employee",
+          "Add role",
+          "Add Department",
           "Exit app",
         ],
       },
@@ -46,16 +46,16 @@ function init() {
       }
     });
 }
-
+// Function that shows all edpartments in the console
 function ViewAllDepartments() {
   db.findAllDepartments()
     .then((records) => {
-      //console.log(records);
       console.table(records[0]);
       init();
     })
     .catch((err) => console.log(err));
 }
+// Function that shows all roles in the console
 function ViewAllRoles() {
   db.findAllRoles()
     .then((records) => {
@@ -65,6 +65,7 @@ function ViewAllRoles() {
     })
     .catch((err) => console.log(err));
 }
+// Function that shows all employees in the console
 function ViewAllEmployees() {
   db.findAllEmployees()
     .then((records) => {
@@ -74,39 +75,80 @@ function ViewAllEmployees() {
     })
     .catch((err) => console.log(err));
 }
-
+// Function that creates a new employee in the database
 function createEmployee() {
-  db.insertEmployee()
-    .then((records) => {
-      //console.log(records);
-      console.table(records[0]);
-      init();
-    })
-    .catch((err) => console.log(err));
-}
-function createRole() {
-  inquirer.prompt([
+  inquirer
+  .prompt([
     {
-      //tilte
+      type: "input",
+      message: "Type your first name",
+      name: "first_name"
     },
     {
-      //salary
+      type: "input",
+      message: "Type your last name",
+      name: "last_name"
     },
     {
-     // department_id 
-     //Option to have list of department Is - select first and then 
-     //Option List of hardcoded departmentId
-     //Option Enter
+      type: "input",
+      message: "What's your role id?",
+      name: "first_name"
+    },
+    {
+      type: "list",
+      message: "What is your department id?",
+      name: "first_name"
     }
+  ]).then(response => {
+    db.insertEmployee()
+      .then((records) => {
+        console.table(records[0]);
+        init();
+      })
+      .catch((err) => console.log(err));
+  })
+}
+
+function createRole() {
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Type your title",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Type your salary",
+      name: "salary",
+    },
+    {
+      type: "list",
+      message: "Choose your department",
+      name: "department_id",
+      choices: [
+        "1",
+        "2",
+        "3"
+      ],
+    },
   ]).then(response => {
   db.insertRole(response.title,response.salary,response.department_id)
     .then((records) => {
-      //console.log(records);
       console.table(records[0]);
       init();
     })
     .catch((err) => console.log(err));
   })
+}
+
+function createDepartment() {
+  db.insertDepartment()
+    .then((records) => {
+      console.table(records[0]);
+      init();
+    })
+    .catch((err) => console.log(err));
 }
 
 function exitApp() {
